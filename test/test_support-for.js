136a52b26a74cd814ad6a3ce7403d1b7396a0761
JohnAlbin <virtually.johnalbin@gmail.com>
2015-11-17 19:02:38 +0800

    Add more tests.

--- test/test_support-for.js
+++ test/test_support-for.js
@@ -1,27 +1,61 @@
 'use strict';
 
-describe('$support-for', function() {
+describe('@function support-for()', function() {
   before(function(done) {
     sassyTest.configurePaths({
       // Path to Sass library.
-      fixtures: path.join(__dirname, 'fixtures')
+      fixtures: path.join(__dirname, 'fixtures/support-for')
     });
     done();
   });
 
-  describe('default values', function() {
-    it('should support last 4 versions of all browsers except IE 8', function(done) {
+  describe('$support-for variable', function() {
+    it('should, by default, support last 4 versions of all browsers except IE 8', function(done) {
       sassyTest.renderFixture('defaults', {}, function(error, result, expectedOutput) {
         should.not.exist(error);
         done();
       });
     });
-  });
 
-  describe('negative values', function() {
     it('should support last X versions when given a negative version', function(done) {
       sassyTest.renderFixture('negative-values', {}, function(error, result, expectedOutput) {
         should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should error when version is not a number', function(done) {
+      sassyTest.renderFixture('error-browser-number', {}, function(error, result, expectedOutput) {
+        error.should.exist;
+        error.message.should.equal('$support-for: (safari: 9) must be set to an integer (or null); 9 is a string.');
+        done();
+      });
+    });
+
+    it('should error when version is not an integer', function(done) {
+      sassyTest.renderFixture('error-browser-integer', {}, function(error, result, expectedOutput) {
+        error.should.exist;
+        error.message.should.equal('$support-for: (ie: 5.5) must be set to an integer (or null); 5.5 is not an integer.');
+        done();
+      });
+    });
+  });
+
+  describe('$browser parameter', function() {
+    it('should error when not in $support-for-current-browser-version', function(done) {
+      sassyTest.renderFixture('error-current-browser', {}, function(error, result, expectedOutput) {
+        error.should.exist;
+        error.message.should.equal('mosaic not found in $support-for-current-browser-version map; it must be set to an integer.');
+        done();
+      });
+    });
+  });
+
+  describe('$version parameter', function() {
+    it('should error when not an integer', function(done) {
+      sassyTest.renderFixture('error-version', {}, function(error, result, expectedOutput) {
+        error.should.exist;
+        error.message.should.equal('The $version parameter of support-for() must be an integer; string given.');
         done();
       });
     });
