689fa9794d84c1381e984daa0a9fe2c1c247bf7e
JohnAlbin <virtually.johnalbin@gmail.com>
2016-04-04 15:31:03 +0800

    Update to sassy-test 3.x.

--- test/helper.js
+++ test/helper.js
@@ -1,15 +1,6 @@
 'use strict';
 
 // Globals for all test_*.js files.
+global.expect = require('chai').expect;
 global.path = require('path');
-global.should = require('chai').should();
-global.sassyTest = require('sassy-test');
-
-// This before() is run before any test_*.js file.
-before(function(done) {
-  sassyTest.configurePaths({
-    // Path to Sass library.
-    includePaths: [path.join(__dirname, '../sass')]
-  });
-  done();
-});
+global.SassyTest = require('sassy-test');
--- test/test_support-for.js
+++ test/test_support-for.js
@@ -1,76 +1,66 @@
 'use strict';
 
+var sassyTest = new SassyTest({
+  includePaths: [path.join(__dirname, '../sass')],
+  // Path to Sass library.
+  fixtures: path.join(__dirname, 'fixtures/support-for')
+});
+
 describe('@function support-for()', function() {
-  before(function(done) {
-    sassyTest.configurePaths({
-      // Path to Sass library.
-      fixtures: path.join(__dirname, 'fixtures/support-for')
-    });
-    done();
-  });
-
   describe('$support-for variable', function() {
-    it('should, by default, support last 4 versions of all browsers except IE 8', function(done) {
-      sassyTest.renderFixture('defaults', {}, function(error, result, expectedOutput) {
-        should.not.exist(error);
-        done();
+    it('should, by default, support last 4 versions of all browsers except IE 8', function() {
+      return sassyTest.renderFixture('defaults');
+    });
+
+    it('should support last X versions when given a negative version', function() {
+      return sassyTest.renderFixture('negative-version');
+    });
+
+    it('should not support a browser when version is null', function() {
+      return sassyTest.renderFixture('null-version');
+    });
+
+    it('should support a browser by using the wildcard \'*\' browser version', function() {
+      return sassyTest.renderFixture('wildcard-browser');
+    });
+
+    it('should error when version is not a number', function() {
+      return sassyTest.renderFixture('error-browser-number').then(function() {
+        throw new Error('An error should have occurred');
+      }).catch(function(error) {
+        expect(error).to.exist;
+        expect(error.message).to.equal('$support-for: (safari: 9) must be set to an integer (or null); 9 is a string.');
       });
     });
 
-    it('should support last X versions when given a negative version', function(done) {
-      sassyTest.renderFixture('negative-version', {}, function(error, result, expectedOutput) {
-        should.not.exist(error);
-        done();
-      });
-    });
-
-    it('should not support a browser when version is null', function(done) {
-      sassyTest.renderFixture('null-version', {}, function(error, result, expectedOutput) {
-        should.not.exist(error);
-        done();
-      });
-    });
-
-    it('should support a browser by using the wildcard \'*\' browser version', function(done) {
-      sassyTest.renderFixture('wildcard-browser', {}, function(error, result, expectedOutput) {
-        should.not.exist(error);
-        done();
-      });
-    });
-
-    it('should error when version is not a number', function(done) {
-      sassyTest.renderFixture('error-browser-number', {}, function(error, result, expectedOutput) {
-        error.should.exist;
-        error.message.should.equal('$support-for: (safari: 9) must be set to an integer (or null); 9 is a string.');
-        done();
-      });
-    });
-
-    it('should error when version is not an integer', function(done) {
-      sassyTest.renderFixture('error-browser-integer', {}, function(error, result, expectedOutput) {
-        error.should.exist;
-        error.message.should.equal('$support-for: (ie: 5.5) must be set to an integer (or null); 5.5 is not an integer.');
-        done();
+    it('should error when version is not an integer', function() {
+      return sassyTest.renderFixture('error-browser-integer').then(function() {
+        throw new Error('An error should have occurred');
+      }).catch(function(error) {
+        expect(error).to.exist;
+        expect(error.message).to.equal('$support-for: (ie: 5.5) must be set to an integer (or null); 5.5 is not an integer.');
       });
     });
   });
 
   describe('$browser parameter', function() {
-    it('should error when not in $support-for-current-browser-version', function(done) {
-      sassyTest.renderFixture('error-current-browser', {}, function(error, result, expectedOutput) {
-        error.should.exist;
-        error.message.should.equal('mosaic not found in $support-for-current-browser-version map; it must be set to an integer.');
-        done();
+    it('should error when not in $support-for-current-browser-version', function() {
+      return sassyTest.renderFixture('error-current-browser').then(function() {
+        throw new Error('An error should have occurred');
+      }).catch(function(error) {
+        expect(error).to.exist;
+        expect(error.message).to.equal('mosaic not found in $support-for-current-browser-version map; it must be set to an integer.');
       });
     });
   });
 
   describe('$version parameter', function() {
-    it('should error when not an integer', function(done) {
-      sassyTest.renderFixture('error-version', {}, function(error, result, expectedOutput) {
-        error.should.exist;
-        error.message.should.equal('The $version parameter of support-for() must be an integer; string given.');
-        done();
+    it('should error when not an integer', function() {
+      return sassyTest.renderFixture('error-version').then(function() {
+        throw new Error('An error should have occurred');
+      }).catch(function(error) {
+        expect(error).to.exist;
+        expect(error.message).to.equal('The $version parameter of support-for() must be an integer; string given.');
       });
     });
   });
